72a86e109e532db4a5a871a43ef5706bed0988c7
Johnny Lieu <johnnylieu@yahoo.com>
2021-02-15 13:36:31 -0800

    form is working

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -2,8 +2,12 @@ exports.renderHomePage = (req, res) => {
     res.render("index", {
         title: "Express Weather Finder by Johnny Lieu"
     });
-};
+}
+
+exports.getWeather = (req, res) => {
+    res.send("your form has been submitted");
+}
 
 exports.renderAboutPage = (req, res) => {
     res.render("about")
-};
+}
