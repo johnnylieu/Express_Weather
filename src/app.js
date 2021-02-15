c72f2b258d1b3a23a5dd89293bad26ca0d6fc5de
Johnny Lieu <johnnylieu@yahoo.com>
2021-02-14 23:56:08 -0800

    mvc

--- src/app.js
+++ src/app.js
@@ -9,14 +9,6 @@ app.set("view engine", "hbs");
 
 port = 3000
 
-app.get("/", (req, res) => {
-res.render("index")
-});
-
-app.get("/about", (req, res) => {
-    res.render("about")
-});
-
 app.listen(port, () => {
     console.log("the server is now running on port " + port)
 });
---
+++ src/router.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.router();
+
+router.get("/", (req, res) => {
+    res.render("index", {
+        title: "Express Weather Finder by Johnny Lieu"
+    })
+});
+
+router.get("/about", (req, res) => {
+    res.render("about")
+});
