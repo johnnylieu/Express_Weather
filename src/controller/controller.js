b878abac24cd3cf80414aee5f03b2fc095944ffa
Johnny Lieu <johnnylieu@yahoo.com>
2021-02-15 12:27:10 -0800

    added title hbs to router

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,5 +1,7 @@
 exports.renderHomePage = (req, res) => {
-    res.render("index")
+    res.render("index", {
+        title: "Express Weather Finder by Johnny Lieu"
+    });
 };
 
 exports.renderAboutPage = (req, res) => {
