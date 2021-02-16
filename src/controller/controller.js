97f632323d14a195770bc1e3ad2c0389cf219f17
Johnny Lieu <johnnylieu@yahoo.com>
2021-02-15 19:01:45 -0800

    added http prefix to api url

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -9,7 +9,7 @@ exports.renderHomePage = (req, res) => {
 exports.getWeather = (req, res) => {
     const city = req.body.city;
     const API_KEY = "8f775258afdec054195f89c38855f678";
-    const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
     axios.get(url).then((response) => {
         console.log(response);
     }).catch((error) => {
